0a26eb12a1f5f2ff6b35d0745c913843da3408d0
Ilham Rofi'i <70249693+ilhamr0f11@users.noreply.github.com>
2022-02-12 00:09:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,12 @@
-# Made with python3
-# (C) @FayasNoushad
-# Copyright permission under MIT License
-# All rights reserved by FayasNoushad
-# License -> https://github.com/FayasNoushad/YouTube-Search-Bot/blob/main/LICENSE
-
-import os
 import ytthumb
-import requests
-from pyrogram import Client, filters
 from youtubesearchpython import VideosSearch
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultPhoto
 
 
-Bot = Client(
-    "YouTube-Search-Bot",
-    bot_token = os.environ.get("BOT_TOKEN"),
-    api_id = int(os.environ.get("API_ID")),
-    api_hash = os.environ.get("API_HASH")
-)
-
-
-@Bot.on_message(filters.private & filters.all)
-async def text(bot, update):
-    text = "Search youtube videos using below buttons.\n\nMade by @FayasNoushad"
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
-            [InlineKeyboardButton(text="Search in another chat", switch_inline_query="")]
-        ]
-    )
-    await update.reply_text(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
-    )
-
-
-@Bot.on_inline_query()
-async def search(bot, update):
-    results = VideosSearch(update.query, limit=50).result()
-    answers = []
+def main():
+    query = input('Enter your query :- ')
+    results = VideosSearch(query, limit=50).result()
     for result in results:
-        title = result["int"]
+        title = result["title"]
         views_short = result["viewCount"]["short"]
         duration = result["duration"]
         duration_text = result["accessibility"]["duration"]
@@ -51,31 +15,13 @@ async def search(bot, update):
         channel_name = result["channel"]["name"]
         channel_link = result["channel"]["link"]
         description = f"{views_short} | {duration}"
-        details = f"**Title:** {title}" + "\n" \
-        f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
-        f"**Duration:** {duration_text}" + "\n" \
-        f"**Views:** {views}" + "\n" \
-        f"**Published Time:** {publishedtime}" + "\n" \
-        "\n" + "**Made by @FayasNoushad**"
-        thumbnail = ytthumb.thumbnail(result["id"])
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton(text="Watch Video 📹", url=result["link"])]
-            ]
-        )
-        try:
-            answers.append(
-                InlineQueryResultPhoto(
-                    title=title,
-                    description=description,
-                    caption=details,
-                    photo_url=thumbnail,
-                    reply_markup=reply_markup
-                )
-            )
-        except:
-            pass
-    await update.answer(answers)
+        details = f"Title: {title}" + "\n" \
+        f"Channel: [{channel_name}] ({channel_link})" + "\n" \
+        f"Duration: {duration_text}" + "\n" \
+        f"Views: {views}" + "\n" \
+        f"Published Time: {publishedtime}" + "\n" \
+        "\n" + ytthumb.thumbnail(result["id"])
+        print(details+'\n')
 
 
-Bot.run()
+main()
